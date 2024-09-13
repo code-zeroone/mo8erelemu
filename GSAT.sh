75f008f8abbdccd76b6bdc730297dfae043d1f7d
Code-01 <64568214+code-zeroone@users.noreply.github.com>
2024-09-13 18:53:22 +0300

    Add files via upload

---
+++ GSAT.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+echo '======================================================'
+echo '                                              		'
+echo '           SCript BY- BAHAA LINUXCODE/n               '
+echo '                               						'
+echo '======================================================'
+wget --no-check-certificate  -q  "dl.dropboxusercontent.com/scl/fi/uy61gjt6xku6g136neelh/enigma2-plugin-softcams-gosatplus_all.ipk?rlkey=k6ryhgwz8v767unqyhh80nuly&st=iwhsrff0&dl=0" -O /tmp/enigma2-plugin-softcams-gosatplus_all.ipk
+opkg remove  --force-remove --force-depends enigma2-plugin-softcams-gosatplus
+opkg install --force-reinstall --force-depends /tmp/enigma2-plugin-softcams-gosatplus_all.ipk
+rm -f /tmp/enigma2-plugin-softcams-gosatplus_all.ipk
+echo '=================================================='
+echo '           INSTALL PLUGIN\n               '
+echo '=================================================='
+wget --no-check-certificate  -q  "dl.dropboxusercontent.com/scl/fi/xws95ncdu6pgpob07zdr3/GosatPlus_plugin.tar.gz?rlkey=5yqfyftm6vt3b0141w9cqdhqf&st=mi4cm2fh&dl=0" -O /tmp/GosatPlus_plugin.tar.gz
+tar -xf /tmp/GosatPlus_plugin.tar.gz -C /tmp/
+rm -r /usr/lib/enigma2/python/Plugins/Extensions/GosatPlus
+cp -r /tmp/GosatPlus /usr/lib/enigma2/python/Plugins/Extensions
+rm -f /tmp/GosatPlus_plugin.tar.gz
+rm -r /tmp/GosatPlus
+echo '=================================================='
+echo '           Activate Server\n               '
+echo '=================================================='
+mac=$(ifconfig eth0 | awk '/HWaddr/ {print $5}')
+# Create the URL
+macUrl="https://api.qrserver.com/v1/create-qr-code/?size=512x512&data=https://e2.gosatplus.com/V3?id=${mac}"
+# Download the QR code image
+wget --no-check-certificate "$macUrl" -O /usr/lib/enigma2/python/Plugins/Extensions/GosatPlus/qr.png
+echo '=================================================='
+echo '           SCript BY- BAHAA LINUXCODE\n               '
+echo '*              Power cam successfully installed                *'
+echo '*                    RESTARTING... Box                         *'
+echo '=================================================='
+init 4
+sleep 2
+init 3
+exit 0
